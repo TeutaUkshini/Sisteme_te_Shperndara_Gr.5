4596de954ac83d3fd5485b44456e18f5d8064b8a
tonikrasniqi <51675513+tonikrasniqi@users.noreply.github.com>
2021-07-07 23:18:21 +0200

    Create babel-config.js
    Eksportimi me vuejs

---
+++ ds-project/frontend/babel-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+    // module: {
+    //     rules: [{
+    //         test: /\.svg$/,
+    //         use: [
+    //             'babel-loader',
+    //             'vue-svg-loader',
+    //         ],
+    //     }, ],
+    // },
+}
