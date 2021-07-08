9c1b04ec32556c9009660703ecf7fc87cc69c75b
TeutaUkshini <44062876+TeutaUkshini@users.noreply.github.com>
2021-07-09 00:53:21 +0100

    Update main.js
    Krijimi i fajllit kryesor

--- ds-project/frontend/src/main.js
+++ ds-project/frontend/src/main.js
@@ -1,1 +1,21 @@
 #Krijimi i main.js
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+axios.defaults.baseURL = 'http://localhost:5001/kidshouse-d646d/us-central1/app/api';
+axios.defaults.headers.common.Accept = 'application/json';
+
+
+Vue.use(VueAxios, axios)
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app')
