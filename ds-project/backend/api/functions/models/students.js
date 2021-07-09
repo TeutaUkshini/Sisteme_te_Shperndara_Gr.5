5f946aacb0abf39bc12ae399df2ebb3b26429306
LirimMuqolli <57811400+LirimMuqolli@users.noreply.github.com>
2021-07-10 00:04:05 +0200

    Create students.js

---
+++ ds-project/backend/api/functions/models/students.js
@@ -0,0 +1,14 @@
+'use strict'
+
+class students {
+  constructor ({ id, name, age, surname, contact, classname }) {
+    this.id = id
+    this.name = name
+    this.age = age
+    this.surname = surname
+    this.contact = contact
+    this.classname = classname
+  }
+}
+
+module.exports = students
